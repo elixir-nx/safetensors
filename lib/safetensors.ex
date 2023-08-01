80ce883f7390dd1ea5073dfbb3790632276a1fa0
Gonzalo <456459+grzuy@users.noreply.github.com>
2023-08-01 13:08:03 -0300

    mix new safetensors

---
+++ lib/safetensors.ex
@@ -0,0 +1,18 @@
+defmodule Safetensors do
+  @moduledoc """
+  Documentation for `Safetensors`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Safetensors.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/safetensors_test.exs
@@ -0,0 +1,8 @@
+defmodule SafetensorsTest do
+  use ExUnit.Case
+  doctest Safetensors
+
+  test "greets the world" do
+    assert Safetensors.hello() == :world
+  end
+end
