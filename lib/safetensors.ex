acf0d2b6f295991f6f185e9a4c98040ab963b85e
Gonzalo <456459+grzuy@users.noreply.github.com>
2023-08-01 14:28:08 -0300

    feat: load data with Safetensors.load!/1

--- lib/safetensors.ex
+++ lib/safetensors.ex
@@ -3,16 +3,55 @@ defmodule Safetensors do
   Documentation for `Safetensors`.
   """
 
-  @doc """
-  Hello world.
+  # https://huggingface.co/docs/safetensors/index#format
 
-  ## Examples
+  @header_metadata_key "__metadata__"
 
-      iex> Safetensors.hello()
-      :world
+  @dtype_mapping %{
+    "BF16" => :bf16,
+    "F64" => :f64,
+    "F32" => :f32,
+    "F16" => :f16,
+    "I64" => :s64,
+    "I32" => :s32,
+    "I16" => :s16,
+    "I8" => :s8,
+    "U64" => :u64,
+    "U32" => :u32,
+    "U16" => :u16,
+    "U8" => :u8
+    # "BOOL" => :u8
+  }
 
-  """
-  def hello do
-    :world
+  def load!(data) when is_binary(data) do
+    <<
+      header_size::unsigned-64-integer-little,
+      header_json::binary-size(header_size),
+      buffer::binary
+    >> = data
+
+    {_metadata, header} =
+      header_json
+      |> Jason.decode!()
+      |> Map.pop(@header_metadata_key)
+
+    header
+    |> Enum.into(%{}, fn {tensor_name, tensor_info} ->
+      %{
+        "data_offsets" => [offset_start, offset_end],
+        "dtype" => dtype,
+        "shape" => shape
+      } = tensor_info
+
+      type = @dtype_mapping[dtype] || raise "unrecognized dtype #{dtype}"
+
+      {
+        tensor_name,
+        buffer
+        |> binary_part(offset_start, offset_end - offset_start)
+        |> Nx.from_binary(type)
+        |> Nx.reshape(List.to_tuple(shape))
+      }
+    end)
   end
 end
--- test/safetensors_test.exs
+++ test/safetensors_test.exs
@@ -2,7 +2,11 @@ defmodule SafetensorsTest do
   use ExUnit.Case
   doctest Safetensors
 
-  test "greets the world" do
-    assert Safetensors.hello() == :world
+  test "load" do
+    # source:
+    # https://github.com/huggingface/safetensors/blob/1a65a3fdebcf280ef0ca32934901d3e2ad3b2c65/bindings/python/tests/test_simple.py#L35-L40
+    serialized = ~s(<\x00\x00\x00\x00\x00\x00\x00{"test":{"dtype":"I32","shape":[2,2],"data_offsets":[0,16]}}\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00)
+
+    assert Safetensors.load!(serialized) == %{"test" => Nx.tensor([[0, 0], [0, 0]], type: :s32)}
   end
 end
