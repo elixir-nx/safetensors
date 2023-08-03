63e626e7770c4e2826c090ed392ffa456fb0cea4
Gonzalo <456459+grzuy@users.noreply.github.com>
2023-08-03 11:01:37 -0300

    test: test coverage for when safetensors file has metadata

--- test/safetensors_test.exs
+++ test/safetensors_test.exs
@@ -10,4 +10,13 @@ defmodule SafetensorsTest do
 
     assert Safetensors.load!(serialized) == %{"test" => Nx.tensor([[0, 0], [0, 0]], type: :s32)}
   end
+
+  test "load with metadata" do
+    # source:
+    # https://github.com/huggingface/safetensors/blob/1a65a3fdebcf280ef0ca32934901d3e2ad3b2c65/bindings/python/tests/test_simple.py#L42-L50
+    serialized =
+      ~s(f\x00\x00\x00\x00\x00\x00\x00{"__metadata__":{"framework":"pt"},"test1":{"dtype":"I32","shape":[2,2],"data_offsets":[0,16]}}       \x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00)
+
+    assert Safetensors.load!(serialized) == %{"test1" => Nx.tensor([[0, 0], [0, 0]], type: :s32)}
+  end
 end
